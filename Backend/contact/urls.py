216aa08b3b9280d6d74dc6f03e7cf38e0462c8d9
OzodbekmeW <OzodbekmeW@users.noreply.github.com>
2026-01-28 10:22:57 +0500

    Backend butunlay o'chirildi va Skills section muammosi tuzatildi

--- Backend/contact/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'contact'
-
-urlpatterns = [
-    path('send/', views.contact_message_create, name='send_message'),
-    path('health/', views.health_check, name='health_check'),
-]
